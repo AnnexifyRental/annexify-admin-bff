86ed66cf5ea6fcefb0bb2d17f3617b2c3d204f30
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-10-17 10:37:26 +0530

    Initial setup

---
+++ src/main/java/com/anuradha/annexifyadminbff/AnnexifyAdminBffApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifyadminbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnexifyAdminBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnexifyAdminBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuradha/annexifyadminbff/AnnexifyAdminBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifyadminbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnexifyAdminBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
